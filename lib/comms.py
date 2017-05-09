eb3321ca85eb50fed1d133ee844b37c12dfa8407
corey-stidston <Corey.Stidston@student.uts.edu.au>
2017-05-09 22:24:49 +1000

    Added AES - CBC encryption, ANSI padding from crypto_utils.py and time stamped messages for protection against replay attacks

--- lib/comms.py
+++ lib/comms.py
@@ -1,7 +1,9 @@
 import struct
-
+import time
 from Crypto.Cipher import XOR
-
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from . import crypto_utils
 from dh import create_dh_key, calculate_dh_secret
 
 class StealthConn(object):
@@ -26,14 +28,29 @@ class StealthConn(object):
             their_public_key = int(self.recv())
             # Obtain our shared secret
             shared_hash = calculate_dh_secret(their_public_key, my_private_key)
-            print("Shared hash: {}".format(shared_hash))
+            #print("Shared hash: {}".format(shared_hash))
+            
+            # my code
+            initialisation_vector = shared_hash[-16:]
+            self.cipher = AES.new(shared_hash[:32], AES.MODE_CBC, initialisation_vector)
+
+            # other method - Counter Mode
+            #ctr = Counter.new(128, initial_value="Randomint")
+            #self.cipher = AES.new(shared_hash[:32], AES.MODE_CTR, ctr) # cipher block chaining
+            
 
         # Default XOR algorithm can only take a key of length 32
-        self.cipher = XOR.new(shared_hash[:4])
+        # self.cipher = XOR.new(shared_hash[:4]) #<----------CHANGE / REMOVE THIS
 
     def send(self, data):
         if self.cipher:
-            encrypted_data = self.cipher.encrypt(data)
+            container = b"".join([data, struct.pack(">i", int(time.time()))])
+
+            padded_message = crypto_utils.ANSI_X923_pad(container, self.cipher.block_size) #self.cipher.block_size
+            
+            #encrypt entire message
+            encrypted_data = self.cipher.encrypt(padded_message)
+
             if self.verbose:
                 print("Original data: {}".format(data))
                 print("Encrypted data: {}".format(repr(encrypted_data)))
@@ -54,14 +71,25 @@ class StealthConn(object):
 
         encrypted_data = self.conn.recv(pkt_len)
         if self.cipher:
-            data = self.cipher.decrypt(encrypted_data)
+
+            #decrypt the message into unpadded form
+            unpadded_message = self.cipher.decrypt(encrypted_data)
+            #unpad the message
+            container = crypto_utils.ANSI_X923_unpad(unpadded_message, self.cipher.block_size)
+            packet_time = struct.unpack(">i", container[-4:])[0]
+            
+            if(time.time() - packet_time > 10):
+                print("\nWARNING: REPLAY ATTACK DETECTED")
+                
+
+            data = container[:-4]
+
             if self.verbose:
                 print("Receiving packet of length {}".format(pkt_len))
                 print("Encrypted data: {}".format(repr(encrypted_data)))
                 print("Original data: {}".format(data))
         else:
             data = encrypted_data
-
         return data
 
     def close(self):
